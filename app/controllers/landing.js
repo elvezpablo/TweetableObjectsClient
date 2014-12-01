385e63a941e965904c6d0a1e3f8bc0eb9a8e60bf
elvezpablo <prangel@gmail.com>
2014-12-01 11:12:35 -0800

    demo complete

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -1,3 +1,10 @@
-module.exports = function($scope) {
+module.exports = ['$scope', '$location', 'Paging', function($scope, $location, Paging) {
+
     console.log("Landing");
-};
+    Paging.setInfo(null, "CONGRATULATIONS! YOUR DEVICE IS READY FOR TWEETS.");
+
+    $scope.start = function() {
+        $location.url("/");
+    }
+
+}];
--- app/controllers/localWifi.js
+++ app/controllers/localWifi.js
@@ -1,25 +1,49 @@
 module.exports = ['$scope', '$location', '$timeout', 'UriMonitor', 'Paging', function($scope ,$location, $timeout, UriMonitor, Paging) {
     console.log("LocalWifi");
+    Paging.setInfo(5,"STEP FIVE: RECONNECTING YOUR COMPUTER")
     // using the
     var url = "https://api-http.littlebitscloud.cc/ping";
     var checkLittleBits = UriMonitor(url, 5000);
-    var WAITING_MSG = "Attempting to connect to local wifi...";
+    var OFFLINE_MESSAGE = "Waiting for network switch...";
+    var WAITING_MSG = "Attempting to reconnect to local wifi...";
     var CONNECTED_MSG = "Connected!";
+    var DEMO = true;
 
-    $scope.message = WAITING_MSG;
+    $scope.message = OFFLINE_MESSAGE;
+
+    var done = function() {
+        checkLittleBits.stop();
+        $location.url("/6");
+        console.log("/6");
+    };
+
+    var setMessage = function() {
+        console.log("set message");
+        $scope.message = CONNECTED_MSG;
+    };
 
     checkLittleBits.on("change", function(e) {
+        console.log("change");
         if(e === true) {
-            $scope.message = CONNECTED_MSG;
-            $timeout(function() {
-                checkLittleBits.stop();
-                $location.path("landing");
-            },2000);
+            setMessage();
+            $timeout(done, 3000);
         }
     });
-    checkLittleBits.start();
 
-//    $timeout(function() {
-//        $location.path("landing");
-//    }, 2500);
+
+    if(DEMO) {
+        $timeout(function() {
+            $scope.message = WAITING_MSG;
+            console.log("starting littlebits");
+            checkLittleBits.start();
+        },3000);
+    } else {
+        var handleOffline = function() {
+            $scope.$apply(function() {
+                $scope.message =  WAITING_MSG;
+                checkLittleBits.start();
+            })
+        };
+        $window.addEventListener("offline",handleOffline);
+    }
 }];
