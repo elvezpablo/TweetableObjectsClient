6c9049cf2d4ad2b3463b509bbc4a384c7f3422b7
elvezpablo <prangel@gmail.com>
2015-01-04 19:06:14 -0800

    cleanup

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,11 +2,7 @@ module.exports = ['$scope', 'Devices', function($scope, Devices) {
     console.log("Admin");
     //console.log($route.current.params);
     // Paging.setInfo(0, "STEP ZERO: TURN ON YOUR DEVICE");
-
-
-
     Devices.list().then(function(devices) {
-        console.log(devices);
         $scope.devices = devices;
     });
 
